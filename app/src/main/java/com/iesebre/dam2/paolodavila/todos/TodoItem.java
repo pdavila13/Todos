49076b765b8ab37a2fa842c9e8fc060159d397ab
Paolo Davila <paolodavila@iesebre.com>
2015-11-13 16:55:26 +0100

    Add xivato toString

--- app/src/main/java/com/iesebre/dam2/paolodavila/todos/TodoItem.java
+++ app/src/main/java/com/iesebre/dam2/paolodavila/todos/TodoItem.java
@@ -8,7 +8,19 @@ public class TodoItem {
     private String name;
     private boolean done;
     private int priority;
-    
+
+    public TodoItem() {
+        this.name = "";
+        this.done = false;
+        this.priority = 1;
+    }
+
+    public TodoItem(String name, boolean done, int priority) {
+        this.name = name;
+        this.done = done;
+        this.priority = priority;
+    }
+
     public String getName() {
         return name;
     }
@@ -31,5 +43,10 @@ public class TodoItem {
 
     public void setPriority(int priority) {
         this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "{ name: " + name + ", done: " + done + ", priority: " + priority + " }";
     }
 }
